c1b277d315f88ee34968bb58b2d809d9e63b3e8d
antoniamikey <antonia-schikora@outlook.de>
2025-04-30 14:13:33 +0200

    first stuff with FastAPI

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    __table_args__ = {'extend_existing': True}
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    is_admin = Column(Boolean, default=False)
