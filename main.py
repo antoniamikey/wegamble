2e7fb39973ba61dee591f5771298decf094b9ea7
antoniamikey <antonia-schikora@outlook.de>
2025-04-30 11:21:53 +0200

    i guess i have to restart

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routers import invite, users
-
-app = FastAPI()
-
-# Integriere die beiden Router (Einladungen und Benutzer)
-app.include_router(invite.router)
-app.include_router(users.router)
